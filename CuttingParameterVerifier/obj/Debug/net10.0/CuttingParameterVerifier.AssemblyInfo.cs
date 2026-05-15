31016c5b96f2e86f2fa5ae373520fb4e92f5daeb
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-05-15 12:50:35 +0800

    Initial full local project upload - overwrite remote

--- CuttingParameterVerifier/obj/Debug/net10.0/CuttingParameterVerifier.AssemblyInfo.cs
+++ CuttingParameterVerifier/obj/Debug/net10.0/CuttingParameterVerifier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3496e849c28c514b2ff581f2c3da7423a88860f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012150b5794676748db16d53cb877b8cc4377988")]
 [assembly: System.Reflection.AssemblyProductAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
