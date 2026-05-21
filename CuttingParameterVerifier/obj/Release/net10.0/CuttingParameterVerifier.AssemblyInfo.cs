68ddc67613200750101f80a402f0dc942410c084
Hakim Hisham <hakim.hisham.1991@gmail.com>
2026-05-21 18:19:17 +0800

    Initial commit - force replace with local state

--- CuttingParameterVerifier/obj/Release/net10.0/CuttingParameterVerifier.AssemblyInfo.cs
+++ CuttingParameterVerifier/obj/Release/net10.0/CuttingParameterVerifier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967381bb56b78315182fcbfbe81853b8ec3a5208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1000aaaf6691a0f3543c857d63221bd69e9ba8dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CuttingParameterVerifier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
